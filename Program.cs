dfa4aac6f26ab418776631944e47ca67fa41e96c
Maria <ec39sp@yandex.ru>
2023-12-29 21:06:01 +0200

    my code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите элементы массива через запятую: ");
+        string input = Console.ReadLine();
+        string[] words = input.Split(',');
+
+        int count = 0;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Trim().Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        int index = 0;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Trim().Length <= 3)
+            {
+                result[index] = words[i].Trim();
+                index++;
+            }
+        }
+        
+        Console.WriteLine("Результат:");
+        foreach (string word in result)
+        {
+            Console.Write(word + " ");
+        }
+    }
+}
